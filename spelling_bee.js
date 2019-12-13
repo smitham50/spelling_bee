c6233d20b17c771e5e3e295c6355486d6e423776
Aaron Smith <smitham50@gmail.com>
2019-12-13 11:49:40 -0500

    start game button chooses two vowels and five consonants and stores them in array

---
+++ spelling_bee.js
@@ -0,0 +1,43 @@
+const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+//start button
+const start = document.querySelector('button');
+
+//game start chooses letters from vowels and consonants and renders on hex grid
+start.addEventListener('click', () => {
+  let gameLetters = [];
+  
+  //call pickVowels function twice to add two unique vowels to gameLetters array
+  for (let i = 0; i < 2; i++) {
+    pickVowels();
+  }
+
+  //pickVowels function recurses until it finds a vowel that isn't already in gameLetters array
+  function pickVowels() {
+    let idx = Math.floor(Math.random() * 5)
+
+    if (!gameLetters.includes(vowels[idx])) {
+      gameLetters.push(vowels[idx]);
+    } else {
+      pickVowels();
+    }
+  }
+
+  //call pickConsonants five times to add five unique consonants to gameLetters array
+  for (let i = 0; i < 5; i++) {
+    pickConsonants();
+  }
+
+  //pickConsonants recurses until it finds a consonant that isn't already in gameLetters array
+  function pickConsonants() {
+    let idx = Math.floor(Math.random() * 20);
+
+    if (!gameLetters.includes(consonants[idx])) {
+      gameLetters.push(consonants[idx]);
+    } else {
+      pickConsonants();
+    }
+  }
+  debugger
+} )
